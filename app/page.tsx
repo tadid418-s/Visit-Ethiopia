284e4d4e6d51a51150d045be9b7d9515d8f79096
tadid418-s <tadiyos.dejene@aastustudent.edu.et>
2025-08-20 03:15:29 +0300

    Removed unwanted sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import About from "@/components/About";
-import Camp from "@/components/Camp";
 import FavoriteDestinations from "@/components/FavoriteDestinations";
-import Features from "@/components/Features";
-import GetApp from "@/components/GetApp";
-import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -12,10 +8,6 @@ export default function Home() {
       <Hero />
       <About />
       <FavoriteDestinations />
-      <Camp />
-      <Guide />
-      <Features />
-      <GetApp />
     </>
   )
 }
